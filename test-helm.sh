8db6d3f892cd71439e19f46602ac4eab82c2662d
LeDangQuocAn <23520008@gm.uit.edu.vn>
2026-06-03 14:05:39 +0700

    add Slack webhook external secret config

--- test-helm.sh
+++ test-helm.sh
@@ -1,6 +1,8 @@
 for svc in account bank credit-card discovery-client gateway invoice log user; do
   echo "Testing $svc..."
-  helm template "test-$svc" charts/banking-spring-boot -f "environments/staging/${svc}-values.yaml" > /dev/null
+  helm template "test-$svc" charts/banking-spring-boot \
+    -f environments/staging/common-values.yaml \
+    -f "environments/staging/${svc}-values.yaml" > /dev/null
   if [ $? -eq 0 ]; then
     echo "✅ $svc: Render thành công"
   else
